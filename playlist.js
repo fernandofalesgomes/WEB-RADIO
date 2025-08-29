ca9aed8df3998b35af5e505053537863e4a01529
fernandofalesgomes <143642507+fernandofalesgomes@users.noreply.github.com>
2025-08-29 03:52:36 -0300

    Delete playlist.js

--- playlist.js
+++
@@ -1,8 +0,0 @@
-// playlist.js
-const musicas = [
-  "ELETRONICA BRITO CARIMBO 01.mp3",
-  "ELETRONICA BRITO CARIMBO 02.mp3",
-  "ELETRONICA BRITO CARIMBO 03.mp3",
-  "00 - aac - TRILHAS ESPORTIVAS NOVAS (4).aac"
-  // adicione novas músicas aqui, apenas com o nome do arquivo
-];
